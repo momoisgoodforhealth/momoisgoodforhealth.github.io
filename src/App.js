e6f005d36a996dcb316fba6d6c3f89f0cfea9571
momoisgoodforhealth <shoban.shakya@gmail.com>
2022-07-03 14:15:58 -0400

    P2.3

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import React, { useState } from 'react'
 const Course = (props) => {
     const cor = props.course
     const parts = cor.parts
+    var totalExercises = parts.reduce(function (sum, part) {
+        return sum + part.exercises
+    },0)
+
     const Heading = () => <div><h1>{cor.name}</h1></div>
     const Content = () => {
         return (parts.map(part =>
-            <div><ul><li>{part.name} {part.exercises}</li></ul></div>))
+            <div>
+                <ul><li>{part.name} {part.exercises}</li></ul>
+                
+            </div>))
     }
+    const Total = () => <div><p1>{totalExercises} </p1></div>
     return(
         <div>
             <Heading />
             <Content />
+            <Total />
         </div>
     )
 
